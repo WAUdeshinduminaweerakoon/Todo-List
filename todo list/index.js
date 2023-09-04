acf9e5920d386fe4c58a1429699250efab7b09d3
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-09-04 17:28:33 +0530

    conect the css file and

--- todo list/index.js
+++ todo list/index.js
@@ -1,3 +1,4 @@
+const bodyParser = require("body-parser");
 const express = require("express")
 const app = express()
 //const badyParser = require("bady-parser")
@@ -6,6 +7,12 @@ const mongoose = require("mongoose")
 const port = 3000
 
 app.set("view engine", "ejs"); 
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+  
+
+const dburl ="mongodb://localhost:27017/tododb"
 
 app.get("/", (request, response) =>{
     response.render("index")
